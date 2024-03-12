d4068373463de4cdf8ff0220408acc2184ffa6b4
Hugo Fonseca <hugo.fonseca@contaduria.marandu.com.ar>
2024-03-12 20:26:08 -0300

    index js modificated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
+}
+
+render();
