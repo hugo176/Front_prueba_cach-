61376d8e8f7500ca5bc7e6f93825e841aff372c1
Hugo Fonseca <hugo.fonseca@contaduria.marandu.com.ar>
2024-03-12 10:41:14 -0300

    Add button comprobar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+//import { useState } from 'react';
 import './App.css';
 import { useSelector } from 'react-redux';
 
 function App() {
 
   const tasksState = useSelector(state => state.tasks);console.log(tasksState);
+ // const [showModal, setShowModal] = useState(false);
+
+  const handleBottonClick = () => {
+    console.log("1"); 
+  };
 
   return (
     <div className="App">
       <h1>Hola v1.1.1</h1>
+      <button onClick={() => handleBottonClick()}>Comprobar</button>
     </div>
   );
 }
